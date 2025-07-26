1e7b1fe14f6e1b8f325a19c105f8050ce4f7c96e
ATHARVA KULKARNI <152779278+koolkarniAtharva@users.noreply.github.com>
2025-07-26 13:56:35 +0530

    04 authentiction ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { SignInview } from "@/modules/auth/ui/views/sign-in-view";
+import React from "react";
+
+const page = () => {
+  return (
+    <SignInview/>
+  );
+};
+
+export default page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpview } from '@/modules/auth/ui/views/sign-up-view'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignUpview/>
+  )
+}
+
+export default page
