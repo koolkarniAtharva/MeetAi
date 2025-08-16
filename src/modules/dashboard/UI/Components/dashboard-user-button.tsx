ee8238c73b4d294fe3ef24dc96d0a7c6277570e7
ATHARVA KULKARNI <152779278+koolkarniAtharva@users.noreply.github.com>
2025-08-16 21:10:55 +0530

    07:Dashboard-Navbar

--- src/modules/dashboard/UI/Components/dashboard-user-button.tsx
+++ src/modules/dashboard/UI/Components/dashboard-user-button.tsx
@@ -1,6 +1,11 @@
-import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 
+import { useIsMobile } from "@/hooks/use-mobile";
+import { authClient } from "@/lib/auth-client"
+
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -8,17 +13,23 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-
-
 } from "@/components/ui/dropdown-menu"
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { GeneratedAvatar } from "@/components/generated-avatar";
-import { useRouter } from "next/navigation";
-
+import{
+    Drawer,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+    
+} from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
     const router = useRouter();
     const {data, isPending} = authClient.useSession();
+    const ismobile = useIsMobile();
 
     const onLogout = ()=>{
         authClient.signOut({
@@ -34,11 +45,60 @@ export const DashboardUserButton = () => {
         return null;
     }
 
-
+    if(ismobile){
+        return(
+            <Drawer>
+                <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+                {data.user.image ? (
+            <Avatar>
+                <AvatarImage src={data.user.image}/>
+            </Avatar>
+        ):(
+            <GeneratedAvatar
+                seed={data.user.name}
+                variant="initials"
+                className="size-9 mr-3"
+            />
+        )}
+        <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1min-w-0 ">
+            <p className="text-sm truncate w-full">
+                {data.user.name}
+            </p>
+            <p className="text-xs truncate w-full">
+                {data.user.email}
+            </p>
+        </div>
+        <ChevronDownIcon className="size-4 shrink-0"/>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{data.user.name}</DrawerTitle>
+                        <DrawerDescription>{data.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button
+                        variant={"outline"}
+                        onClick={()=>{}}
+                        >
+                            <CreditCardIcon className="size-4 text-black"/>
+                            Billing
+                        </Button>
+                        <Button
+                        variant={"outline"}
+                        onClick={onLogout}
+                        >
+                            <LogOutIcon className="size-4 text-black"/>
+                            Logout
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
 
     return (
     <DropdownMenu>
-        <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+        <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
         {data.user.image ? (
             <Avatar>
                 <AvatarImage src={data.user.image}/>
